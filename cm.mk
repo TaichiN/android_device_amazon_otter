226f6df05a148e84e1146b8a534ccca05df5c93d
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2012-12-06 15:37:14 +0900

    Use mini tablet config

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@
 TARGET_BOOTANIMATION_NAME := 600
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product-if-exists, vendor/cm/config/common_mini_tablet_wifionly.mk)
 
 # Inherit device configuration for Kindle Fire
 $(call inherit-product, device/amazon/otter/full_otter.mk)
